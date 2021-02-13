05b3103c8f1a45f4907e47191498991b509cf8ea
Kendshi <anton_gamayunov@mail.ru>
2021-02-13 21:23:07 +0300

    interact object
    Добавили интерактивные объекты

---
+++ HealthBar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private Vector3 sliderOffset;
+    [SerializeField] private Transform enemyPos;
+    [SerializeField] private Text enemyName;
+    [SerializeField] private EnemyData enemyData;
+    [SerializeField] private Vector3 nameOffset;
+    [SerializeField] private string enterName;
+    
+    private void Start()
+    {
+        if (enemyData != null)
+        {
+            enemyName.text = enemyData.enemyName;
+        }
+        else enemyName.text = enterName;
+    }
+
+    private void Update()
+    {
+        _slider.transform.position = Camera.main.WorldToScreenPoint(enemyPos.position + sliderOffset);
+        enemyName.transform.position = Camera.main.WorldToScreenPoint(enemyPos.position + nameOffset);
+    }
+
+    public void SetHealthValue(float currentHealth, float maxHealth)
+    {
+        _slider.gameObject.SetActive(currentHealth < maxHealth);
+        _slider.value = currentHealth;
+        _slider.maxValue = maxHealth;
+    }
+}
---
+++ Interactable.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Интерактивный объект
+/// </summary>
+public class Interactable : MonoBehaviour
+{
+    [Tooltip("Флажок активности")]
+    public bool IsActive = true;
+    [Tooltip("Радиус взаимодействия")]
+    public float Radius = 5f;
+    [Header("Сюда помещаем то действие которое хотим совершить")]
+    public UnityEvent ActionEvent;
+
+    private Outline outline;
+
+
+    private void Start()
+    {
+        outline = gameObject.AddComponent<Outline>();
+        outline.OutlineColor = Color.yellow;
+        outline.OutlineWidth = 2;
+        outline.enabled = false;
+    }
+
+    /// <summary>
+    /// Виртуальный метод взаимодействия
+    /// </summary>
+    protected virtual void Interact(GameObject character)
+    {
+        Debug.Log("Interactable : Interact with " + gameObject.name);
+        ActionEvent.Invoke();
+    }
+
+    /// <summary>
+    /// Иницализация взаимодействия
+    /// </summary>
+    public void InitInteraction(GameObject character)
+    {
+        if (IsActive)   // проверка флажка активности
+        {
+            if (Vector3.Distance(character.transform.position, gameObject.transform.position) <= Radius)    // проверка расстояния до персонажа
+            {
+                Interact(character); // взаимодействие
+            }
+        }
+    }
+
+    private void OnMouseEnter()
+    {                           
+        outline.enabled = true;     //Если наводим мышку то подсвечиваем контур объекта
+    }
+
+    private void OnMouseExit()
+    {
+        outline.enabled = false;    //Если убираем мышку подсветка исчезает
+    }
+}
